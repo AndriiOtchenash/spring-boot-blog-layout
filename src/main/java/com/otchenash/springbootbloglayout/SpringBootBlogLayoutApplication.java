766a8373249a0cdb5818aecf16e0b326f1b607df
Andrii Otchenash <andrii.otchenash@gmail.com>
2020-12-08 23:29:40 +0200

    created main layout

---
+++ src/main/java/com/otchenash/springbootbloglayout/SpringBootBlogLayoutApplication.java
@@ -0,0 +1,13 @@
+package com.otchenash.springbootbloglayout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBlogLayoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBlogLayoutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/otchenash/springbootbloglayout/SpringBootBlogLayoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otchenash.springbootbloglayout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBlogLayoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
